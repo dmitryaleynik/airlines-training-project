2f91df6a93b6afb779f946ed52927c821cae406d
screamcha <scream1234540@gmail.com>
2018-01-26 10:04:44 +0300

    BrowserRouter instead of HashRouter

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,36 +1,35 @@
+// @flow
 import React from 'react';
-import { Route, NavLink, HashRouter, } from 'react-router-dom';
+import { Route, Link, } from 'react-router-dom';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
 
 const App = () => (
-  <HashRouter>
-    <div className="container">
-      <nav className="navbar navbar-light">
-        <NavLink className="navbar-brand" to="/">
-          Airlines
-        </NavLink>
-        <ul className="navbar-nav" id="navbarNav">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/sign-up">
-              Sign up
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/sign-in">
-              Sign in
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <div className="content row justify-content-center">
-        <Route exact path="/" component={Home} />
-        <Route path="/sign-up" component={SignUp} />
-        <Route path="/sign-in" component={SignIn} />
-      </div>
+  <div className="container">
+    <nav className="navbar navbar-light">
+      <Link className="navbar-brand" to="/">
+        Airlines
+      </Link>
+      <ul className="navbar-nav" id="navbarNav">
+        <li className="nav-item">
+          <Link className="nav-link" to="/sign-up">
+            Sign up
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/sign-in">
+            Sign in
+          </Link>
+        </li>
+      </ul>
+    </nav>
+    <div className="content row justify-content-center">
+      <Route exact path="/" component={Home} />
+      <Route path="/sign-up" component={SignUp} />
+      <Route path="/sign-in" component={SignIn} />
     </div>
-  </HashRouter>
+  </div>
 );
 
 export default App;
--- client-app/src/index.js
+++ client-app/src/index.js
@@ -1,8 +1,14 @@
-/* global document */
+// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  (document.getElementById('root'): any)
+);
