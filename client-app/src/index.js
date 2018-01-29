d6e5cf3ff403b1c236a05516970ee3194e65e34e
screamcha <scream1234540@gmail.com>
2018-01-29 09:22:36 +0300

    redux added

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -1,8 +1,18 @@
 /* global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, } from 'redux';
+import { Provider, } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.css';
-import App from './App';
+import clientApp from 'src/reducers';
+import App from 'src/components/App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(clientApp);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
