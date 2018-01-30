5ec09977d20983e863097f7495aa4a1223696c8f
screamcha <scream1234540@gmail.com>
2018-01-30 13:17:33 +0300

    adding fields in FlightFinder implemented

--- client-app/src/components/NewFlight/FlightFinder/index.js
+++ client-app/src/components/NewFlight/FlightFinder/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { Component, } from 'react';
 
-const FlightFinder = (props) => {
-  return (
-    <div>
-      <h2>Step 1: Find a flight</h2>
-      <input type="text" placeholder="airport" onChange={props.findFlight} />
-    </div>
-  );
-};
+class FlightFinder extends Component {
+  state = {
+    filterFields: [],
+  };
+
+  addFilterField = (e, props) => {
+    this.setState({
+      filterFields: [
+        ...this.state.filterFields,
+        <input
+          key={this.state.filterFields.length}
+          type="text"
+          placeholder="airport"
+          onChange={props.findFlight}
+        />,
+      ],
+    });
+    return;
+  };
+
+  render() {
+    const addFilterField = (e) => {
+      return this.addFilterField(e, this.props);
+    };
+
+    return (
+      <div>
+        <h2>Step 1: Find a flight</h2>
+        <button onClick={addFilterField}>Add field</button>
+        {this.state.filterFields.map((field) => field)}
+      </div>
+    );
+  }
+}
 
 export default FlightFinder;
