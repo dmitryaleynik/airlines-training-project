e28f912c898ce9f5c3eb1d055877942f9e5c4e68
screamcha <scream1234540@gmail.com>
2018-02-12 15:02:47 +0300

    monr Modal container changes

--- client-app/src/components/Modal/container.js
+++ client-app/src/components/Modal/container.js
@@ -1,16 +1,9 @@
 import { connect, } from 'react-redux';
-import { openModal, closeModal, } from 'src/actions/modals';
+import { closeModal, } from 'src/actions/modals';
 import Modal from 'src/components/Modal';
 
-const mapStateToProps = (state) => {
-  return {
-    modal: state.modals.modal,
-  };
-};
-
 const mapDispatchToProps = {
-  openModal,
   closeModal,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Modal);
+export default connect(null, mapDispatchToProps)(Modal);
