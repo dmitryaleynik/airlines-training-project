e2818cd2dee4eaa864a578eaaac7825fcd43b961
screamcha <scream1234540@gmail.com>
2018-02-20 09:55:36 +0300

    ErrorPage + header & footer changes

---
+++ client-app/src/components/ErrorPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = (props) => {
+  return (
+    <div className="error-page text-center">
+      <h2 className="mt-5">404 page not found!</h2>
+      <h2>Try another page.</h2>
+    </div>
+  );
+};
+
+export default ErrorPage;
