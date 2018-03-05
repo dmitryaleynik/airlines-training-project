6ffdb96635741982de0eb4186d6ae8eb1ae0d276
screamcha <scream1234540@gmail.com>
2018-03-05 10:44:38 +0300

    stored procedure for psql

--- server/src/DataAccessLayer/orders.js
+++ server/src/DataAccessLayer/orders.js
@@ -2,7 +2,7 @@ const client = require('./setup/postgres');
 const Order = require('../contracts/orders');
 
 const getAllOrders = async () => {
-  const query = 'SELECT * FROM orders';
+  const query = 'SELECT * from get_all_orders()';
   const result = await client.query(query);
   return result.rows.map(row => {
     return new Order(row.status, row.leave_at, row.total);
