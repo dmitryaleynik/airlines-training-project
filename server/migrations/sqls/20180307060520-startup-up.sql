51de76d1a8c193b9487f6785d45fad4ccbfd44af
screamcha <scream1234540@gmail.com>
2018-03-07 14:59:57 +0300

    sql scripts are added

--- server/migrations/sqls/20180307060520-startup-up.sql
+++ server/migrations/sqls/20180307060520-startup-up.sql
@@ -7,6 +7,12 @@ CREATE TYPE "order" AS
   total INTEGER
 );
 
+CREATE TYPE user_main_info AS
+(
+  user_id integer,
+  email character varying(255)
+);
+
 -- tables --
 CREATE TABLE orders
 (
@@ -15,6 +21,7 @@ CREATE TABLE orders
   status VARCHAR(255) NOT NULL,
   total INTEGER NOT NULL
 );
+
 CREATE TABLE users
 (
   user_id SERIAL PRIMARY KEY,
@@ -32,15 +39,26 @@ BEGIN
   FROM orders;
 END;
 $$ LANGUAGE plpgsql;
+
 CREATE FUNCTION get_user_by_email(user_email VARCHAR(255))
 RETURNS INTEGER AS $$
 BEGIN
-  RETURN (SELECT user_id FROM users WHERE user_email=email);
+  return (SELECT user_id FROM users WHERE user_email=email);
 END;
 $$ LANGUAGE plpgsql;
+
 CREATE FUNCTION insert_user(user_email VARCHAR(255), user_hash TEXT, user_salt TEXT)
 RETURNS VOID AS $$
 BEGIN
   INSERT INTO users(email, password_hash, password_salt) VALUES(user_email, user_hash, user_salt);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION get_user_by_email(user_email character varying)
+  RETURNS user_main_info AS $$
+declare ret user_main_info;
+BEGIN
+  SELECT user_id, email into ret FROM users WHERE user_email=email;
+  return ret;
 END;
 $$ LANGUAGE plpgsql;
