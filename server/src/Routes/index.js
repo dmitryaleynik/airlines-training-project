40d8f4dabf67f1153719d2224fcdef7c99354fd6
screamcha <scream1234540@gmail.com>
2018-03-21 10:30:14 +0300

    changes in booking endpoints

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -3,11 +3,13 @@ const profileRouter = require('./profile');
 const regRouter = require('./registration');
 const authRouter = require('./authorization');
 const newFlightRouter = require('./newFlight');
+const bookingRouter = require('./booking');
 
 const router = new Router();
 router.use(profileRouter.routes());
 router.use(regRouter.routes());
 router.use(authRouter.routes());
 router.use(newFlightRouter.routes());
+router.use(bookingRouter.routes());
 
 module.exports = router;
