09af6daeb0fbb79de8605b058fbcb40a07cd244f
screamcha <scream1234540@gmail.com>
2018-03-27 11:55:21 +0300

    custom thunk middleware 2.0

---
+++ client-app/src/middlewares/thunk.js
@@ -0,0 +1,30 @@
+import { AUTHORIZATION_LOGOUT, AUTHORIZATION_SUCCESS, } from 'src/actions/types';
+import { TOKEN, } from 'src/imports';
+
+export default (store) => (next) => async (action) => {
+  if (typeof action === 'function') {
+    try {
+      await action(store.dispatch, store.getState);
+    } catch (err) {
+      if (err.response && err.response.status === 401) {
+        handleUnauthorized(store.dispatch);
+      } else {
+        throw err;
+      }
+    }
+  } else {
+    if (action.type === AUTHORIZATION_SUCCESS) {
+      handleAuthSuccess(action.payload);
+    }
+    return next(action);
+  }
+};
+
+const handleUnauthorized = (dispatch) => {
+  localStorage.removeItem(TOKEN);
+  dispatch({ type: AUTHORIZATION_LOGOUT, });
+};
+
+const handleAuthSuccess = (token) => {
+  localStorage.setItem(TOKEN, token);
+};
