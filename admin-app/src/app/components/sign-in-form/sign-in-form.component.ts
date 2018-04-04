04681f0bd10d4b217f8a569ace1eb62ad72f8e4a
screamcha <scream1234540@gmail.com>
2018-04-04 16:24:25 +0300

    template and styles naming

--- admin-app/src/app/components/sign-in-form/sign-in-form.component.ts
+++ admin-app/src/app/components/sign-in-form/sign-in-form.component.ts
@@ -3,14 +3,14 @@ import { SignInRequest } from '../../classes/sign-in.request';
 
 @Component({
   selector: 'app-sign-in-form',
-  templateUrl: './sign-in-form.component.html',
-  styleUrls: ['./sign-in-form.component.less'],
+  templateUrl: './sign-in-form.template.html',
+  styleUrls: ['./sign-in-form.styles.less'],
 })
 export class SignInFormComponent implements OnInit {
   @Input() user: SignInRequest;
   @Input() onSubmit: Function;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 }
