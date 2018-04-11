3e3a34aeee24cab9ff24ee6a6582e3472f53454e
screamcha <scream1234540@gmail.com>
2018-04-11 15:15:12 +0300

    api url as env variable

--- admin-app/src/constants.ts
+++ admin-app/src/constants.ts
@@ -1,6 +1,8 @@
+import { environment } from './environments/environment';
+
 export const AUTH_TOKEN = 'authToken';
 
-export const SERVER_URL = 'http://localhost:3001/admin';
+export const SERVER_URL = environment.API_URL;
 
 export const DATETIME_FORMAT = 'YYYY-MM-DDTHH:mm';
 
