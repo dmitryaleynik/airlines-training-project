9cb3815fba743121cf0bbd6f7d85d10dff140d6d
screamcha <scream1234540@gmail.com>
2018-04-11 15:54:38 +0300

    change READMEs

--- admin-app/src/config/jwtModule.ts
+++ admin-app/src/config/jwtModule.ts
@@ -2,6 +2,6 @@ import { AUTH_TOKEN, SERVER_URL } from '../constants';
 
 export const jwtModuleConfig = {
   tokenGetter: () => localStorage.getItem(AUTH_TOKEN),
-  whitelistedDomains: [ 'localhost:3001' ],
-  blacklistedRoutes: ['http://localhost:3001/admin/sign-in']
+  whitelistedDomains: [new URL(SERVER_URL).host],
+  blacklistedRoutes: [`${SERVER_URL}/sign-in`],
 };
